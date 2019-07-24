6a366f1cadc7b4a0ff8af70fc3bb68c082a61bd9
Max Heydasch <max@staff-202-99.eduroam.unibe.ch>
2019-07-24 12:51:43 +0200

    identifier can now be passed as a list

--- rename_FoV.py
+++ rename_FoV.py
@@ -18,18 +18,18 @@ import string
 import random
 import pandas as pd
 import argparse
-
+#%%
 def parseArguments():
   # Define the parser and read arguments
   parser = argparse.ArgumentParser(description='collect segmentation files into one directory')
   parser.add_argument('-d', '--dir', type=str, help='The directory where the files are', required=True)
-  parser.add_argument('-i', '--identifier', type=str, help='The identifier for the files', required=False)
+  parser.add_argument('-i', '--identifier', type=str, help='A list of possible identifier for the files', required=True)
 
   args = parser.parse_args()
   return(args)
 
-def rename_files(path, identifier=None):  
-    identifierpattern=re.compile('[A-Z]{3,}', flags=re.IGNORECASE) #identifyimg tif files with a channelname
+def rename_files(path, identifier):  
+    #identifierpattern=re.compile('[A-Z]{3,}', flags=re.IGNORECASE) #identifyimg tif files with a channelname
     os.chdir(path)    
     os.listdir(path)
     onlyfiles=[f for f in os.listdir(path) if isfile(join(path, f))]    
@@ -49,36 +49,35 @@ def rename_files(path, identifier=None):
     
     #recompiling names in the required order and saving them to new folder
     for i in onlyfiles:
-        if identifier == None:
-            identifier=identifier=re.search(identifierpattern, i).group()  #identifying relevant files
-        if identifier in i:
-            print(i, 'renamed to:')
-            #identifier=re.search(identifierpattern, i).group().strip('.tif') 
-            fov = re.search(fovpattern, i).group()
-            #date = re.search(datepattern, i).group().replace("_", "")
-            well=re.search(wellpattern, i).group().strip('_') 
-            #time=re.search(timepattern, i).group()
-            file = os.path.join(path, i)
-            #creates a 3 characters long random string of uppercase letters and digits and replaces the wellname with it
-            if fov in new_fovs_list:
-                new_fov=new_fovs_list[fov]
-            else:
-                while new_fov in new_fovs_list.values():
-                    new_fov=''.join(random.choices(string.digits, k=4))
-                    new_fov='F'+new_fov
-            new_fovs_list.update({fov: new_fov})
-            oldlocation=os.path.join(path, i)
-            newname=new_fov+'_'+identifier+'_'+well+'.tif'
-            newlocation=os.path.join(path, newname) 
-            print(newname)  
-            os.rename(oldlocation, newlocation)
+        for iden in identifier:
+           
+            if iden in i:
+                
+                print(i, 'renamed to:')
+                #identifier=re.search(identifierpattern, i).group().strip('.tif') 
+                fov = re.search(fovpattern, i).group()
+                #date = re.search(datepattern, i).group().replace("_", "")
+                well=re.search(wellpattern, i).group().strip('_') 
+                #time=re.search(timepattern, i).group()
+                file = os.path.join(path, i)
+                #creates a 3 characters long random string of uppercase letters and digits and replaces the wellname with it
+                if fov in new_fovs_list:
+                    new_fov=new_fovs_list[fov]
+                else:
+                    while new_fov in new_fovs_list.values():
+                        new_fov=''.join(random.choices(string.digits, k=4))
+                        new_fov='F'+new_fov
+                new_fovs_list.update({fov: new_fov})
+                oldlocation=os.path.join(path, i)
+                newname=new_fov+'_'+iden+'_'+well+'.tif'
+                newlocation=os.path.join(path, newname) 
+                print(newname)  
+                os.rename(oldlocation, newlocation)
 
 if __name__ == '__main__':
     args=parseArguments()
     path=args.dir
     identifier=args.identifier
     print(args)
-    if identifier != None:
-        rename_files(path, identifier)
-    else:
-        rename_files(path)
+
+    rename_files(path, identifier)
